dc28a252e18cfb84f85dd39da94db2b1b0e2c9a0
KicsiG83 <balogh.gergely83@gmail.com>
2023-07-07 12:02:20 +0200

    Demo missing files

---
+++ demo-modul/src/main/java/jenkins/demo/demo/modul/hello/Hello.java
@@ -0,0 +1,15 @@
+package jenkins.demo.demo.modul.hello;
+
+/**
+ * Hello Word for Jenkins demo
+ *
+ * @author GergelyBalogh
+ * @since 0.1.0
+ */
+public class Hello {
+
+    public void helloJenkins() {
+        System.out.println("Hello Jenkins demo");
+    }
+
+}
